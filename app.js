2ad7e6cc5d9868ca0a7c7d5d8743b0284a424620
melon_dxy <melon_dxy@126.com>
2023-08-15 11:02:33 +0800

    [lesson 04] v-bind&v-html

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ new Vue({
     el : '#vue-app',
     data : {
         name : 'melon',
-        job : 'superman'
+        job : 'superman',
+        website : 'http://baidu.com',
+        websiteTag : '<a href="http://baidu.com">baidu page</a>'
     },
     methods : {
         greet: function (time) {
