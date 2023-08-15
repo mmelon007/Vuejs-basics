5650527f86483606f73022c596593c5a41083e53
melon <976532505@qq.com>
2023-08-15 23:37:03 +0800

    [lesson 22] 简单的vue app

---
+++ ninja-app/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <app-header></app-header>
+    <ninja-content v-bind:ninjas="ninjas"></ninja-content>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import Ninja from './components/Ninja.vue'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      ninjas: [
+              {name: 'Ryu', speciality: 'Vue Components', show: false},
+              {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
+              {name: 'Hitoshi', speciality: 'Click Events', show: false},
+              {name: 'Tango', speciality: 'Conditionals', show: false},
+              {name: 'Kami', speciality: 'Webpack', show: false},
+              {name: 'Yoshi', speciality: 'Data Diggin', show: false}
+          ]
+    }
+  },
+  components: {
+    'app-header' : Header,
+    'ninja-content' : Ninja,
+    'app-footer': Footer
+  }
+
+}
+
+</script>
+
+<style>
+
+body {
+  margin: 0;
+  font-family: 'Nunito SemiBold';
+}
+
+</style>
---
+++ ninja-app/src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+    </header>
+</template>
+  
+<script>
+export default {
+    name: 'app-header',
+    data() {
+        return {
+            title: 'Welcome to Melon Ninja Site'
+        }
+    }
+}
+</script>
+  
+<style scoped>
+
+header {
+    background: lightgreen;
+    padding: 10px;
+}
+
+h1 {
+    color: #222;
+    text-align: center;
+}
+
+</style>
+  
