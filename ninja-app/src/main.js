fcbfb945c5f2ed5f29ead3597ad3f56a7e2f90ab
melon <976532505@qq.com>
2023-08-17 00:07:44 +0800

    [lesson 34] 自定义vue指令

--- ninja-app/src/main.js
+++ ninja-app/src/main.js
@@ -2,7 +2,28 @@ import Vue from "vue";
 import App from "./App.vue";
 import VueResource from "vue-resource";
 
-Vue.use(VueResource)
+Vue.use(VueResource);
+
+Vue.directive('rainbow',{
+  bind(el) {
+    el.style.color = '#' + Math.random().toString().slice(2, 8);
+  }
+});
+
+Vue.directive('theme', {
+  bind(el, binding){
+    if(binding.value == 'wide'){
+      el.style.maxWidth = '1200px';
+    } else if (binding.value == 'narrow') {
+      el.style.maxWidth = '560px';
+    }
+
+    if(binding.arg == 'colunm') {
+      el.style.backgroundColor = '#ddd';
+      el.style.padding = '20px';
+    }
+  }
+})
 
 new Vue({
   render: (h) => h(App),
