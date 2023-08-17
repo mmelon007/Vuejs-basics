4e4494163ebdb8190c5199626532bf85e0eb41f0
melon_dxy <melon_dxy@126.com>
2023-08-17 11:17:13 +0800

    [lesson42] route参数

--- ninja-app/src/components/appHeader.vue
+++ ninja-app/src/components/appHeader.vue
@@ -1,39 +1,39 @@
 <template>
-    <nav>
-        <ul>
-            <li><router-link to="/" exact>Blogs</router-link></li>
-            <li><router-link to="/add" exact>Add Blog</router-link></li>
-        </ul>
-    </nav>
+  <nav>
+    <ul>
+      <li><router-link to="/" exact>Blogs</router-link></li>
+      <li><router-link to="/add" exact>Add Blog</router-link></li>
+    </ul>
+  </nav>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
 
 <style scoped>
-ul{
-    list-style-type: none;
-    text-align: center;
-    margin: 0;
+ul {
+  list-style-type: none;
+  text-align: center;
+  margin: 0;
 }
-li{
-    display: inline-block;
-    margin: 0 10px;
+li {
+  display: inline-block;
+  margin: 0 10px;
 }
-a{
-    color: #fff;
-    text-decoration: none;
-    padding: 6px 8px;
-    border-radius: 10px;
+a {
+  color: #fff;
+  text-decoration: none;
+  padding: 6px 8px;
+  border-radius: 10px;
 }
-nav{
-    background: #444;
-    padding: 14px 0;
-    margin-bottom: 40px;
+nav {
+  background: #444;
+  padding: 14px 0;
+  margin-bottom: 40px;
 }
 .router-link-active {
-    background-color: #eee;
-    color: #444;
+  background-color: #eee;
+  color: #444;
 }
 </style>
