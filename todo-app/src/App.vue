5804ffed7711bd6c668aca71e5d1b145a7c17568
melon_dxy <melon_dxy@126.com>
2023-08-18 10:24:51 +0800

    [Optimize] 改变组件结构，使用compute和watch属性进行优化

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
-    <todo-list :todos="todos" @updateTodos="updateTodoItems()"></todo-list>
-    <done-list :dones="dones"></done-list>
+    <todo-list
+      :todos="todos"
+      :dones="dones"
+      @updateTodos="updateTodoItems()"
+    ></todo-list>
     <add-todo
       :isDisabled="isDisabled"
       @updateTodos="updateTodoItems()"
@@ -11,51 +14,66 @@
 
 <script>
 import todoList from "./components/todoList.vue";
-import doneList from "./components/doneList.vue";
 import addTodo from "./components/addTodo";
 
 export default {
   name: "App",
   components: {
     "todo-list": todoList,
-    "done-list": doneList,
     "add-todo": addTodo,
   },
   data() {
     return {
-      todos: [],
-      dones: [],
+      todoItems: [],
+      isDisabled: false,
     };
   },
   created() {
     this.updateTodoItems();
   },
+  computed: {
+    todos: function () {
+      let todos = [];
+      for (let item in this.todoItems) {
+        if (!this.todoItems[item].finished) {
+          todos.push(this.todoItems[item]);
+        }
+      }
+      return todos;
+    },
+    dones: function () {
+      let dones = [];
+      for (let item in this.todoItems) {
+        if (this.todoItems[item].finished) {
+          dones.push(this.todoItems[item]);
+        }
+      }
+      return dones;
+    },
+  },
   methods: {
     updateTodoItems: function () {
+      this.todoItems = [];
       this.$http
         .get("https://vue-basic-64381-default-rtdb.firebaseio.com/todos.json")
         .then(function (data) {
           return data.json();
         })
         .then(function (data) {
-          let doneArray = [];
-          let todoArray = [];
           for (let key in data) {
             data[key].id = key;
-            if (data[key].finished) {
-              doneArray.push(data[key]);
-            } else {
-              todoArray.push(data[key]);
-            }
+            this.todoItems.push(data[key]);
           }
-          this.dones = doneArray;
-          this.todos = todoArray;
         });
     },
   },
-  computed: {
-    isDisabled: function () {
-      return this.todos.length >= 10;
+  watch: {
+    todos: function () {
+      if (this.todos.length >= 10) {
+        this.isDisabled = true;
+      } else {
+        this.isDisabled = false;
+      }
     },
   },
 };
--- todo-app/src/components/todoList.vue
+++ todo-app/src/components/todoList.vue
@@ -1,43 +1,80 @@
 <template>
-  <div id="todo-list">
-    <label>待办列表</label>
-    <ul>
-      <li v-for="todo in todos" :key="todo.id">
-        <button v-on:click.prevent="updateTodoStatu(todo)">完成</button>
-        {{ todo.description }}
-        <label v-on:click="changeStarState(todo)">star</label>
-        <button v-if="!todo.star" v-on:click.prevent="deleteTodo(todo.id)">delete</button>
-      </li>
-    </ul>
+  <div id="list">
+    <div id="todo-list">
+      <label>待办列表</label>
+      <ul>
+        <li v-for="todo in todos" :key="todo.id">
+          <button v-on:click.prevent="updateTodoState(todo)">完成</button>
+          {{ todo.description }}
+          <label v-on:click="changeStarState(todo)">star</label>
+          <button v-if="!todo.star" v-on:click.prevent="deleteTodo(todo.id)">
+            delete
+          </button>
+        </li>
+      </ul>
+    </div>
+    <div id="done-list">
+      <label>已办列表</label>
+      <ul>
+        <li v-for="done in dones" :key="done.id">
+          {{ done.description }}
+          <label v-if="done.star">star</label>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["todos"],
+  props: ["todos", "dones"],
   data() {
     return {};
   },
   methods: {
     changeStarState: function (todo) {
       todo.star = !todo.star;
-    },
-    deleteTodo: function(id) {
-        this.$http.delete('https://vue-basic-64381-default-rtdb.firebaseio.com/todos/'+ id +'.json').then(
-            function(){
-                this.$emit("updateTodos");
-            }
-        );
-    },
-    updateTodoStatu: function(todo){
-        todo.finished = !todo.finished;
-        let id = todo.id;
-        delete todo.id;
-        this.$http.put('https://vue-basic-64381-default-rtdb.firebaseio.com/todos/'+ id +'.json', todo).then(function(data){
-            console.log(data);
-            this.$emit("updateTodos");
+      let id = todo.id;
+      delete todo.id;
+      this.$http
+        .put(
+          "https://vue-basic-64381-default-rtdb.firebaseio.com/todos/" +
+            id +
+            ".json",
+          todo
+        )
+        .then(function (data) {
+          console.log(data);
+          this.$emit("updateTodos");
         });
-    }
+    },
+    deleteTodo: function (id) {
+      this.$http
+        .delete(
+          "https://vue-basic-64381-default-rtdb.firebaseio.com/todos/" +
+            id +
+            ".json"
+        )
+        .then(function () {
+          this.$emit("updateTodos");
+        });
+    },
+    updateTodoState: function (todo) {
+      todo.finished = !todo.finished;
+      let id = todo.id;
+      delete todo.id;
+      this.$http
+        .put(
+          "https://vue-basic-64381-default-rtdb.firebaseio.com/todos/" +
+            id +
+            ".json",
+          todo
+        )
+        .then(function (data) {
+          console.log(data);
+          this.$emit("updateTodos");
+        });
+    },
   },
 };
 </script>
